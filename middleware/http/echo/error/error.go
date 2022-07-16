ed07f273da8e5745c9d000a2271572acd03494fc
Takuo Oki <takuo.liamg@gmail.com>
2022-07-16 22:36:51 +0900

    implement echo error handling (#13)
    Co-authored-by: takuo.oki <takuo.oki@optim.co.jp>

--- middleware/http/echo/error/error.go
+++ middleware/http/echo/error/error.go
@@ -3,9 +3,13 @@
 package echo_error
 
 import (
-	"google.golang.org/grpc/codes"
+	"context"
+	"fmt"
+	"net/http"
 
 	echo "github.com/labstack/echo/v4"
+	"google.golang.org/grpc/codes"
+
 	"github.com/takuoki/golib/apperr"
 	"github.com/takuoki/golib/applog"
 )
@@ -13,11 +17,18 @@ import (
 func Middleware(internalServerErrorCode string, logger applog.Logger) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			err := next(c)
-			if err != nil {
+			if err := next(c); err != nil {
 				e, ok := apperr.Extract(err)
 				if !ok {
-					e = newInternalServerError(internalServerErrorCode, err)
+					if herr, ok := err.(*echo.HTTPError); ok {
+						e = apperr.NewClientError(
+							codeFromHTTPStatus(c.Request().Context(), herr.Code, logger),
+							"-",
+							fmt.Sprintf("%v", herr.Message),
+						)
+					} else {
+						e = newInternalServerError(internalServerErrorCode, err)
+					}
 				}
 				if e.Log() != "" {
 					logger.Error(c.Request().Context(), e.Log())
@@ -44,4 +55,36 @@ func newInternalServerError(code string, err error) apperr.Err {
 		"internal server error",
 		err.Error(),
 	)
+}
+
+func codeFromHTTPStatus(ctx context.Context, status int, logger applog.Logger) codes.Code {
+	switch status {
+	case http.StatusOK, http.StatusCreated, http.StatusAccepted, http.StatusNoContent:
+		return codes.OK
+	case http.StatusRequestTimeout:
+		return codes.Canceled
+	case http.StatusBadRequest:
+		return codes.InvalidArgument
+	case http.StatusGatewayTimeout:
+		return codes.DeadlineExceeded
+	case http.StatusNotFound:
+		return codes.NotFound
+	case http.StatusConflict:
+		return codes.AlreadyExists
+	case http.StatusForbidden:
+		return codes.PermissionDenied
+	case http.StatusUnauthorized:
+		return codes.Unauthenticated
+	case http.StatusTooManyRequests:
+		return codes.ResourceExhausted
+	case http.StatusNotImplemented:
+		return codes.Unimplemented
+	case http.StatusInternalServerError:
+		return codes.Internal
+	case http.StatusServiceUnavailable:
+		return codes.Unavailable
+	}
+
+	logger.Warnf(ctx, "unknown HTTP status: %d", status)
+	return codes.Internal
 }
